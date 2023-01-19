42065ffb9e6a6ccd752b966e19538031a12fd9dd
hamza harrass <hamzaharrass05@gmail.com>
2023-01-19 09:43:30 +0100

    crees la class user in the class we have tow function (singup,login

---
+++ class/user.php
@@ -0,0 +1,42 @@
+<?php
+    require_once "../config/connection.php";
+    require_once "../config/UserService.php";
+
+    class user {
+        
+        public $id;
+        public $name;
+        public $email;
+        public $password;
+
+        public function __construct($id, $name, $email, $password){
+            $this->id = $id;
+            $this->name = $name;
+            $this->email = $email;
+            $this->password = $password;
+        }
+
+        public static function signUp($name, $email, $password){
+            global $conn;
+            $query = "INSERT INTO `users`(`name`, `email`, `password`) VALUES ('$name','$email','$password')";
+            $stmt = $conn->prepare($query);
+            
+            if($stmt->execute()){
+                $_SESSION["signUpMessage-success"] = "Account has been created successfully!";
+                header("location: ../dashboard.php");
+            }else{
+                $_SESSION["signUpMessage-field"] = "Sorry email or name already taken.";
+                header("location: ../index.php");
+            }
+            
+        }
+        public function login(){
+            $_SESSION["id"] = $this->id;
+            $_SESSION["name"] = $this->name;
+            $_SESSION["email"] = $this->email;
+            $_SESSION["password"] = $this->password;
+            $_SESSION["loginMessage-success"] = "welcome back ". $this->name;
+            header("location: ../dashboard.php");
+        }
+    }
+?>
